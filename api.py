df39d3167e702183ab3461a252e40e1205576a60
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-07-02 19:13:29 -0700

    added some tables, not seeded or migrated

--- api.py
+++ api.py
@@ -1,9 +1,43 @@
 import flask
 from flask import request, jsonify
-from config.py import 
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-# app = flask.Flask(__name__)
-# app.config['DEBUG'] = True
+app = flask.Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+...
+# Configs
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# Modules
+db = SQLAlchemy(app)
+
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    # posts = db.relationship('Post', backref='author')
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+class Post(db.Model):
+    __tablename__ = 'posts'
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
 
 
 if __name__ == '__main__':
